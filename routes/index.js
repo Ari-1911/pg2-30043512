7b12fec512369165b4737db178916d52bf68f87e
Ari-1911 <ariannaalejandrasilvapereira@gmail.com>
2024-05-21 17:34:13 -0400

    Ordenando......

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-=======
-const ContactosController = require("/Controllers/ContactosController.js");
-const contactosController = new ContactosController();
-
->>>>>>> 5ba9dee0568b1da6842875d97bf3d4313c38701a
 var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "¡¡Hola Mundo!!" });
+  res.render("index", { title: "Curriculum" });
 });
 
 module.exports = router;
